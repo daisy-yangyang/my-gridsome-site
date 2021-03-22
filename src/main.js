8c228afb9404035325d3536190c872e5cac837dc
Daisy <15210711533@163.com>
2021-03-22 09:45:21 +0800

    second commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import './assets/index.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.mixin({
+    data() {
+      return {
+        GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
+      }
+    }
+  })
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,155 @@
 <template>
   <Layout>
+    <!-- Page Header -->
+    <header class="masthead" 
+    :style="{
+      backgroundImage: `url(${ GRIDSOME_API_URL + general.cover.url})`
+    }">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{ general.title}}</h1>
+              <span class="subheading">{{general.subtitle}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">{{ edge.node.title }}</h2>
+              <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <g-link
+                href="#"
+              >{{ edge.node.created_by.firstname + edge.node.created_by.lastname }}</g-link>
+              on {{ edge.node.created_at }}
+            </p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <g-link :to="'/tag/' + tag.id">{{ tag.title }}</g-link>
+                &nbsp;&nbsp;
+              </span>
+            </p>
+            <hr />
+          </div>
+          <!-- <div class="post-preview">
+            <a href="post.html">
+              <h2
+                class="post-title"
+              >I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 18, 2019
+            </p>
+          </div>
+          <hr />
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">Science has not yet mastered prophecy</h2>
+              <h3
+                class="post-subtitle"
+              >We predict too much for the next year and yet far too little for the next ten.</h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#">Start Bootstrap</a>
+              on August 24, 2019
+            </p>
+          </div>
+          <hr />
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">Failure is not an option</h2>
+              <h3
+                class="post-subtitle"
+              >Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#">Start Bootstrap</a>
+              on July 8, 2019
+            </p>
+          </div>-->
+          <!-- <hr /> -->
+          <!-- Pager -->
+          <!-- <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div> -->
+          <pager :info="$page.posts.pageInfo"></pager>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+query ($page: Int) {
+  posts: allStrapiPost (perPage: 2, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        created_by {
+          id
+          firstname
+          lastname
+        }
+        tags {
+          id
+          title
+        }
+        created_at
+      }
+    }
+  }
+
+  general: allStrapiGeneral {
+    edges {
+      node {
+        id
+        title
+        subtitle
+        cover {
+          url
+        }
+      }
+    }
   }
 }
+</page-query>
+
+<script>
+import { Pager } from 'gridsome'
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+  name: 'HomePage',
+  components: {
+    Pager
+  },
+  computed: {
+    general () {
+      return this.$page.general.edges[0].node
+    }
+  }
+};
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+<style></style>
